ba6f09027a340b216583ffba10c363ae697b277c
Old Liu <446247645@qq.com>
2018-09-09 21:41:09 +0800

    Update main.py
    can avoid upper title

--- main.py
+++ main.py
@@ -31,12 +31,37 @@ def GetPDFName(root):
     interpreter.process_page(next(page))
     layout = device.get_result()
     text = []
+    avrpagefontsize = CalcAvrPageFrontsize(layout)
     for out in layout:
         if isinstance(out, LTTextBoxHorizontal):
             results = out.get_text()
+            avrfontsize = CalcAvrLineFrontsize(out)
+        if avrfontsize > (avrpagefontsize*6/5):
             if re.match(r'\w\w', results) or re.match(r'\w\s\w\w', results):
                 text.append(results)
     return text[0]
+
+def CalcAvrPageFrontsize(char):
+    avrsize = 0
+    line_num = 0
+    for out in char:
+        if isinstance(out, LTTextBoxHorizontal):
+            results = out.get_text()
+            avrsize += CalcAvrLineFrontsize(out)
+            line_num += 1
+    avrpagefontsize =  avrsize / line_num
+    return avrpagefontsize
+
+def CalcAvrLineFrontsize(char):
+    size = 0
+    char_num = 0
+    for char in char._objs:
+        for c in char._objs:
+            if isinstance(c,LTChar):
+                size += c.size
+                char_num += 1
+    avrlinefontsize = size / char_num
+    return avrlinefontsize
 
 def RenamePDF():
     list = os.listdir(root)
